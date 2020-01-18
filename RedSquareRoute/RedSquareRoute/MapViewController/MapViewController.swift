b829dfbe77708350aaf04a26ddb17a2e62c920a1
AndruhaPower <pazit@yandex.ru>
2020-01-18 15:04:26 +0300

    initial commit

---
+++ RedSquareRoute/RedSquareRoute/MapViewController/MapViewController.swift
@@ -0,0 +1,21 @@
+//
+//  MapViewController.swift
+//  RedSquareRoute
+//
+//  Created by Andrew on 18/01/2020.
+//  Copyright © 2020 Andrew. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .cyan
+    }
+
+
+}
+

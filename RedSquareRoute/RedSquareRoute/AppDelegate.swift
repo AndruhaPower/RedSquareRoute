fe29cab061ee033e7053f08653b23f0f4686605c
AndruhaPower <pazit@yandex.ru>
2020-01-19 04:07:45 +0300

    настройка параметров для доступа к серверам гугл

--- RedSquareRoute/RedSquareRoute/AppDelegate.swift
+++ RedSquareRoute/RedSquareRoute/AppDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import GoogleMaps
+import GooglePlaces
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        GMSServices.provideAPIKey("AIzaSyCIC8NLQ72X0hnqCbttfS2y3R9wCDbDjCo")
+        GMSPlacesClient.provideAPIKey("AIzaSyCIC8NLQ72X0hnqCbttfS2y3R9wCDbDjCo")
         // Override point for customization after application launch.
         return true
     }

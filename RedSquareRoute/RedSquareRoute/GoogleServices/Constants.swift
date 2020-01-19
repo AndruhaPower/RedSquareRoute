798778ebdf7a8aba63159f984dabee41373ccdf0
AndruhaPower <pazit@yandex.ru>
2020-01-19 04:09:52 +0300

    Константы

---
+++ RedSquareRoute/RedSquareRoute/GoogleServices/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  RedSquareRoute
+//
+//  Created by Andrew on 19/01/2020.
+//  Copyright © 2020 Andrew. All rights reserved.
+//
+
+import Foundation
+import CoreLocation
+
+struct Constants {
+    
+    static let googleAPIKey: String = "AIzaSyCIC8NLQ72X0hnqCbttfS2y3R9wCDbDjCo"
+    static let redSquareLocation: CLLocationCoordinate2D = CLLocationCoordinate2D(latitude: 55.754088, longitude: 37.620564)
+    static let zoom: Float = 15
+}
